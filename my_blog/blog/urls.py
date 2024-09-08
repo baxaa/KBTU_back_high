510268372adbcd967dc36cf18f1b49f8c99f942b
Baqbergen <onalbekovbakbergen7@gmail.com>
2024-09-08 22:14:45 +0500

    exercise 5, 6, 7 done

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('blog/', blog, name='blog')
+    path('blog/', blog, name='blog'),
+    path('posts', post_list, name='post_list'),
+    path('posts/<int:post_id>/', post_detail, name='post_detail'),
+    path('post/create/', post_create, name='post_create'),
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
 
 
 # Create your views here.
 def blog(request):
     return "Hello World!"
+
+
+# List view for all blog posts
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')  # Get all posts, ordered by newest first
+    return render(request, 'post_list.html', {'posts': posts})
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)  # Retrieve post by ID or return 404 if not found
+    return render(request, 'post_detail.html', {'post': post})
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the new post to the database
+            return redirect('post_list')  # Redirect to the list of posts after submission
+    else:
+        form = PostForm()
+    return render(request, 'post_create.html', {'form': form})
